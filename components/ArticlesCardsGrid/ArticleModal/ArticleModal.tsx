ae437c1f4a748ebfa445f8a2b7a7ebd38f1ec1d3
guillermo <guillermotays14@gmail.com>
2023-12-19 13:28:19 -0500

    feature: fix key attribute issue

--- components/ArticlesCardsGrid/ArticleModal/ArticleModal.tsx
+++ components/ArticlesCardsGrid/ArticleModal/ArticleModal.tsx
@@ -26,67 +26,69 @@ export function ArticleModal({
   const linkProps = { href: article.web_url, target: '_blank', rel: 'noopener noreferrer' };
   const theme = useMantineTheme();
   return (
-    <Modal
-      opened={opened}
-      onClose={close}
-      title="Article Details"
-      size="auto"
-      centered
-      transitionProps={{ transition: 'fade', duration: 300, timingFunction: 'linear' }}
-    >
-      <Card withBorder radius="md" className={classes.card}>
-        <Card.Section>
-          <a {...linkProps}>
-            <AspectRatio ratio={1920 / 1080}>
-              {article.multimedia[0]?.url ? (
-                <Image src={`https://static01.nyt.com/${article.multimedia[0]?.url}`} />
-              ) : (
-                <Skeleton />
-              )}
-            </AspectRatio>
-          </a>
-        </Card.Section>
+    <>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Article Details"
+        size="auto"
+        centered
+        transitionProps={{ transition: 'fade', duration: 300, timingFunction: 'linear' }}
+      >
+        <Card withBorder radius="md" className={classes.card}>
+          <Card.Section>
+            <a {...linkProps}>
+              <AspectRatio ratio={1920 / 1080}>
+                {article.multimedia[0]?.url ? (
+                  <Image src={`https://static01.nyt.com/${article.multimedia[0]?.url}`} />
+                ) : (
+                  <Skeleton />
+                )}
+              </AspectRatio>
+            </a>
+          </Card.Section>
 
-        <Badge
-          className={classes.rating}
-          variant="gradient"
-          gradient={{ from: 'yellow', to: 'red' }}
-        >
-          Travel News
-        </Badge>
+          <Badge
+            className={classes.rating}
+            variant="gradient"
+            gradient={{ from: 'yellow', to: 'red' }}
+          >
+            Travel News
+          </Badge>
 
-        <Text className={classes.title} fw={500} component="a" {...linkProps}>
-          {article.headline.main}
-        </Text>
+          <Text className={classes.title} fw={500} component="a" {...linkProps}>
+            {article.headline.main}
+          </Text>
 
-        <Text fz="sm" c="dimmed" lineClamp={4}>
-          {article.lead_paragraph}
-        </Text>
+          <Text fz="sm" c="dimmed" lineClamp={4}>
+            {article.lead_paragraph}
+          </Text>
 
-        <Group justify="space-between" className={classes.footer}>
-          <Center>
-            <Avatar
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
-              size={24}
-              radius="xl"
-              mr="xs"
-            />
-            <Text fz="sm" inline>
-              {article.byline.original}
-            </Text>
-          </Center>
+          <Group justify="space-between" className={classes.footer}>
+            <Center>
+              <Avatar
+                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png"
+                size={24}
+                radius="xl"
+                mr="xs"
+              />
+              <Text fz="sm" inline>
+                {article.byline.original}
+              </Text>
+            </Center>
 
-          <Group gap={8} mr={0}>
-            {article.keywords.map((keyword: Keyword) =>
-              keyword.name === 'subject' ? (
-                <Badge color={theme.primaryColor}>{keyword.value}</Badge>
-              ) : (
-                <></>
-              )
-            )}
+            <Group gap={8} mr={0}>
+              {article.keywords
+                .filter((value: Keyword) => value.name === 'subject')
+                .map((keyword: Keyword) => (
+                  <Badge color={theme.primaryColor} key={keyword.value}>
+                    {keyword.value}
+                  </Badge>
+                ))}
+            </Group>
           </Group>
-        </Group>
-      </Card>
-    </Modal>
+        </Card>
+      </Modal>
+    </>
   );
 }
